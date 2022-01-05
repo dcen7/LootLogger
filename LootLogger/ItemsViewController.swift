46be6fd19aada1fe405c652b681025a121ec7425
dcen7 <mdcengiz@gmail.com>
2022-01-05 11:08:58 -0600

    add tableview

---
+++ LootLogger/ItemsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Mehmet Deniz Cengiz on 1/5/22.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
