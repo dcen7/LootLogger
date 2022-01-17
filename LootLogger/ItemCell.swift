b0b645e989fbf48eaf0f446c8e4059fa0921b376
dcen7 <mdcengiz@gmail.com>
2022-01-17 12:48:13 -0600

    add serial number to custom cell and auto label size adjustment

---
+++ LootLogger/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Mehmet Deniz Cengiz on 1/17/22.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
