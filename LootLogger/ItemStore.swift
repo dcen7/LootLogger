4f781ba9607cb3c0d8c3e655303144388923c1e9
dcen7 <mdcengiz@gmail.com>
2022-01-30 09:53:18 -0600

    add saveChanges method to save item info into disk

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -10,6 +10,11 @@ import UIKit
 class ItemStore {
     
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("item.plist")
+    }()
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -33,5 +38,20 @@ class ItemStore {
         let movedItem = allItems[fromIndex]
         allItems.remove(at: fromIndex)
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL)")
+        
+        do{
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allItems)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all of the items")
+            return true
+        } catch let encodingError {
+            print("Error encoding all items \(encodingError)")
+            return false
+        }
     }
 }
